0d115e1c1808e92bda983aaa9c8e8a072f94735c
IgrisModz <kirito59320@gmail.com>
2025-11-08 14:06:44 +0100

    Remove useless popups options

--- Scan Manga/MauiProgram.cs
+++ Scan Manga/MauiProgram.cs
@@ -1,7 +1,5 @@
 ﻿using CommunityToolkit.Maui;
 using Scan_Manga.Services;
-using CommunityToolkit.Maui.Alerts;
-using Microsoft.Maui.Controls.Shapes;
 
 #if DEBUG
 using Microsoft.Extensions.Logging;
@@ -16,36 +14,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiCommunityToolkit(static options =>
-            {
-                options.SetPopupDefaults(new DefaultPopupSettings
-                {
-                    CanBeDismissedByTappingOutsideOfPopup = true,
-                    BackgroundColor = Colors.Transparent,
-                    Margin = 10,
-                    Padding = 0
-                });
-
-                options.SetPopupOptionsDefaults(new DefaultPopupOptionsSettings
-                {
-                    CanBeDismissedByTappingOutsideOfPopup = true,
-                    OnTappingOutsideOfPopup = async () => await Toast.Make("Popup Dismissed").Show(CancellationToken.None),
-                    PageOverlayColor = Color.FromArgb("#80000000"),
-                    Shadow = new Shadow
-                    {
-                        Brush = Colors.Black,
-                        Offset = new Point(10, 10),
-                        Opacity = 0.5f,
-                        Radius = 25
-                    },
-                    Shape = new RoundRectangle
-                    {
-                        CornerRadius = new CornerRadius(20),
-                        Stroke = Colors.Gray,
-                        StrokeThickness = 1
-                    }
-                });
-            })
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -55,7 +24,7 @@ public static class MauiProgram
         builder.Services.AddSingleton<ISystemBarsService, SystemBarsService>();
 
 #if DEBUG
-            builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
         return builder.Build();
