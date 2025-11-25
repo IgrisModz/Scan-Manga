c034bb1a2a305ef0cb136f8998061227a6f8f366
IgrisModz <kirito59320@gmail.com>
2025-11-25 15:58:31 +0100

    Ajustement des attributs de l'activité principale
    - Suppression de l'import inutile de `Android.OS`.
    - Reformattage de l'attribut `[Activity]` pour plus de lisibilité.
    - Ajout de `ScreenOrientation = ScreenOrientation.SensorPortrait`
      pour forcer l'orientation en mode portrait.
    - Aucun changement fonctionnel dans la logique de `MainActivity`.

--- Scan Manga/Platforms/Android/MainActivity.cs
+++ Scan Manga/Platforms/Android/MainActivity.cs
@@ -1,10 +1,14 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace Scan_Manga
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(
+        Theme = "@style/Maui.SplashTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+        ScreenOrientation = ScreenOrientation.SensorPortrait
+    )]
     public class MainActivity : MauiAppCompatActivity
     {
     }
