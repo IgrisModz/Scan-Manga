a40fdd2e97e9c4421df6d69289786a651557203c
IgrisModz <kirito59320@gmail.com>
2025-11-28 14:37:58 +0100

    Changement vers le nouveau service specifique a la platforme

--- Scan Manga/MauiProgram.cs
+++ Scan Manga/MauiProgram.cs
@@ -21,27 +21,27 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
-            .UseMaterialMauiIcons()
-            .UseFontAwesomeBrandMauiIcons()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            })
-            .ConfigureMauiHandlers(handlers =>
-            {
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMaterialMauiIcons()
+                .UseFontAwesomeBrandMauiIcons()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
 #if ANDROID
-                handlers.AddHandler<CustomWebView, CustomWebViewHandler>();
+                    handlers.AddHandler<CustomWebView, CustomWebViewHandler>();
 #elif IOS
                 handlers.AddHandler<CustomWebView, CustomWebViewHandler>();
 #endif
-            });
+                });
 
 #if ANDROID
-        builder.Services.AddSingleton<ISystemBarsService, SystemBarsService>();
+            builder.Services.AddSingleton<IFullScreenService, AndroidFullscreenService>();
 #endif
 
 #if DEBUG
