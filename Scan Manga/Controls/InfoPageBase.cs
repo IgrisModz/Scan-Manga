2d526794a3af7f7f44d4b11a079cb75def29aa75
IgrisModz <kirito59320@gmail.com>
2025-12-23 23:13:25 +0100

    Refactor: gestion de la StatusBar déplacée dans InfoPageBase
    Suppression du StatusBarBehavior dans les XAML des pages d’information et centralisation de sa gestion dans le constructeur C# de InfoPageBase. Les couleurs de la barre d’état sont désormais appliquées dynamiquement selon le thème. Nettoyage des imports toolkit devenus inutiles.

--- Scan Manga/Controls/InfoPageBase.cs
+++ Scan Manga/Controls/InfoPageBase.cs
@@ -1,13 +1,44 @@
-﻿using Scan_Manga.Services;
+﻿using CommunityToolkit.Maui.Behaviors;
+using CommunityToolkit.Maui.Core;
+using Scan_Manga.Services;
 
 namespace Scan_Manga.Controls;
 
-public class InfoPageBase(IFullScreenService fullScreenService) : ContentPage
+public class InfoPageBase : ContentPage
 {
-    private readonly IFullScreenService _fullScreenService = fullScreenService;
+    private readonly IFullScreenService _fullScreenService;
 
     public InfoPageBase() : this(ServiceHelper.Services.GetRequiredService<IFullScreenService>())
     {
+    }
+
+    public InfoPageBase(IFullScreenService fullScreenService)
+    {
+        _fullScreenService = fullScreenService;
+
+        if (OperatingSystem.IsAndroidVersionAtLeast(23) || OperatingSystem.IsIOSVersionAtLeast(15))
+        {
+            var statusBarBehavior = new StatusBarBehavior { StatusBarStyle = StatusBarStyle.Default };
+
+            var lightColor = Colors.White;
+            if (Application.Current?.Resources.TryGetValue("White", out var whiteObj) == true && whiteObj is Color whiteColor)
+            {
+                lightColor = whiteColor;
+            }
+
+            var darkColor = Color.FromArgb("#1F1F1F");
+            if (Application.Current?.Resources.TryGetValue("OffBlack", out var offBlackObj) == true && offBlackObj is Color offBlackColor)
+            {
+                darkColor = offBlackColor;
+            }
+
+            statusBarBehavior.SetAppThemeColor(
+                StatusBarBehavior.StatusBarColorProperty,
+                lightColor,
+                darkColor
+            );
+            this.Behaviors.Add(statusBarBehavior);
+        }
     }
 
     protected override void OnAppearing()
